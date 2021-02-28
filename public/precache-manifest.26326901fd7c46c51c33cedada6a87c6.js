41d350d206a1705b8043fbdcfb53f4ba3b486af6
claudiiafg <claudia@bliinx.com>
2021-02-28 11:18:24 -0500

    first push

---
+++ public/precache-manifest.26326901fd7c46c51c33cedada6a87c6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716a885bf34233cde054535cbbbf5727",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c980ea8a7c877c36d49a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5adca339c1c04d999934",
+    "url": "/static/css/main.25544442.chunk.css"
+  },
+  {
+    "revision": "c980ea8a7c877c36d49a",
+    "url": "/static/js/2.f899ffc2.chunk.js"
+  },
+  {
+    "revision": "8b100a7b55f28fe17e2d63acef462aea",
+    "url": "/static/js/2.f899ffc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5adca339c1c04d999934",
+    "url": "/static/js/main.96081ebe.chunk.js"
+  },
+  {
+    "revision": "6e72a2f2c9b5b6142a0a",
+    "url": "/static/js/runtime-main.df75913a.js"
+  },
+  {
+    "revision": "df828ede04757402f2367d4000a17546",
+    "url": "/static/media/homepage-bingo-icon.df828ede.svg"
+  },
+  {
+    "revision": "79723be7efae50d17505a21c962d17d1",
+    "url": "/static/media/homepage-boy-computer-icon.79723be7.svg"
+  },
+  {
+    "revision": "81695ba5f6de6e4fd07a01abfd7c0ccf",
+    "url": "/static/media/homepage-girl-computer-icon.81695ba5.svg"
+  },
+  {
+    "revision": "bfd562565100264fbc793932df1f22bb",
+    "url": "/static/media/homepage-lady.bfd56256.svg"
+  },
+  {
+    "revision": "7a44935f844182159622de700e638300",
+    "url": "/static/media/logo.7a44935f.png"
+  }
+]);
